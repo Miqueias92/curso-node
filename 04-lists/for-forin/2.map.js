3b06a613852cea8196acda47bf4c5da6ee94d46b
Miqueias dos Santos Sousa Assad <miqueias.sousa@olisaude.com.br>
2022-05-04 01:03:02 -0300

    usando forEach e map

---
+++ 04-lists/for-forin/2.map.js
@@ -0,0 +1,23 @@
+const service = require('./service')
+
+async function main() {
+    try {
+        const results = await service.obterPessoas(`a`)
+        // const names = []
+
+        // results.results.forEach(function(item){
+        //     names.push(item.name)  
+        // })
+
+        // const names = results.results.map(function(pessoa) {
+        //     return pessoa.name
+        // })
+
+        // const names = results.results.map((pessoa) => pessoa.name)
+        console.log('name', names)
+    } catch (error) {
+        console.error(`DEU RUIM`,error)
+    }
+}
+
+main()
