ff8fb7a502922c35ae4bd3483b5a2149142f10b3
Miqueias Sousa <mi.queias2010@hotmail.com>
2022-05-04 01:14:36 -0300

    criando meu proprio map

--- 04-lists/for-forin/2.map.js
+++ 04-lists/for-forin/2.map.js
@@ -1,5 +1,14 @@
 const service = require('./service')
 
+Array.prototype.meuMap = function (callback) {
+    const novoArrayMapeado = []
+    for(let indice=0; indice <= this.length-1; indice++){
+        const resultado = callback(this[indice], indice)
+        novoArrayMapeado.push(resultado)
+    }
+    return novoArrayMapeado
+}
+
 async function main() {
     try {
         const results = await service.obterPessoas(`a`)
@@ -14,6 +23,10 @@ async function main() {
         // })
 
         // const names = results.results.map((pessoa) => pessoa.name)
+        
+        const names = results.results.meuMap(function(pessoa, indice) {
+            return `[${indice}]${pessoa.name}`
+        })
         console.log('name', names)
     } catch (error) {
         console.error(`DEU RUIM`,error)
