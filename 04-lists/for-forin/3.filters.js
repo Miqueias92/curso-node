95ca46d05d68d8ae667d3f4f4ed4cd745254283a
Miqueias Sousa <mi.queias2010@hotmail.com>
2022-05-05 23:25:43 -0300

    criando meu proprio filter

--- 04-lists/for-forin/3.filters.js
+++ 04-lists/for-forin/3.filters.js
@@ -13,24 +13,38 @@ const {obterPessoas} = require('./service')
  * 
  */
 
+Array.prototype.myFilter = function(callback) {
+    const lista = []
+    for (index in this) {
+        const item = this[index]
+        const result = callback(item, index, this)
+        // 0, "", null, undefined == false
+        if(!result) continue;
+        lista.push(item)
+    }
+    return lista
+}
+
 async function main(){
     try {
 
         const { 
             results 
-        } = await obterPessoas(` a`)
+        } = await obterPessoas(`a`)
 
-        const familiaLars = results.filter(function(item) {
-            // por padrão precisa retornar um boolean
-            // para informar se deve manter ou remover da lista
-            // false > remove da lista
-            // true mantem
-            // não encontrou -1
-            // encontrou = posicaoArray
-            const result = item.name.toLowerCase().indexOf('lars') !== -1
-            return result
-        })
+        // const familiaLars = results.filter(function(item) {
+        //     // por padrão precisa retornar um boolean
+        //     // para informar se deve manter ou remover da lista
+        //     // false > remove da lista
+        //     // true mantem
+        //     // não encontrou -1
+        //     // encontrou = posicaoArray
+        //     const result = item.name.toLowerCase().indexOf('lars') !== -1
+        //     return result
+        // })
 
+        const familiaLars = results.myFilter((item, index, lista) => 
+            item.name.toLowerCase().indexOf('lars') !== -1)
         
         const names = familiaLars.map((pessoa) => pessoa.name)
         console.log(names)
