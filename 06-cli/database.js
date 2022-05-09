e5c66e88c5914b07d7b88e37f2e2e77d2cff8043
Miqueias Sousa <mi.queias2010@hotmail.com>
2022-05-09 00:24:26 -0300

    Manipulando arquivos Json READ

---
+++ 06-cli/database.js
@@ -0,0 +1,36 @@
+const {
+    readFile 
+} = require('fs')
+
+const {
+    promisify
+} = require('util')
+
+const readFileAsync = promisify(readFile)
+
+// outra forma de obter dados json
+// const dadosJson = require('./herois.json')
+
+class Database {
+    
+    constructor() {
+        this.NOME_ARQUIVO = 'herois.json'
+    }
+    
+    async obterDadosArquivo(){
+        const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
+        return JSON.parse(arquivo.toString())
+    }
+
+    escreverArquivo() {
+
+    }
+    
+    async listar(id) {
+        const dados = await this.obterDadosArquivo()
+        const dadosFiltrado = dados.filter(item => id ? (item.id === id) : true)
+        return dadosFiltrado
+    }
+}
+
+module.exports = new Database()
