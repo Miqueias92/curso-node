db5f012647696bcb4088fa352af7b591e6dd40ca
Miqueias Sousa <mi.queias2010@hotmail.com>
2022-07-23 01:27:14 -0300

    connect with postgres using Sequileze

--- 08-multi-db-postgres/src/postgresExample.js
+++ 08-multi-db-postgres/src/postgresExample.js
@@ -2,4 +2,50 @@
  // npm install pg-hstore pg
 
  const Sequelize = require('sequelize')
- const driver = new Sequelize()
+ const driver = new Sequelize(
+     'heroes',
+     'miqueias',
+     '123', {
+        host: 'localhost',
+        dialect: 'postgres',
+        quoteIdentifiers: false, // não vai ser case sensi
+        opeatorsAliases: false
+     }
+ )
+
+ async function main(params) {
+    const Herois = driver.define('herois', {
+        id: {
+            type: Sequelize.INTEGER,
+            require: true,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: {
+            type: Sequelize.STRING,
+            require: true
+        },
+        poder: {
+            type: Sequelize.STRING,
+            require: true
+        },
+    }, {
+        tableName: 'TB_HEROIS',
+        freezeTableName: false,
+        timestamps: false
+    })
+    await Herois.sync()
+
+    await Herois.create({
+        nome: 'Lanterna Verde',
+        poder: 'Anel'
+    })
+    
+    const result = await Herois.findAll({
+        raw: true,
+        attributes: ['nome']
+    })
+    console.log('result', result)
+ }
+
+ main()
