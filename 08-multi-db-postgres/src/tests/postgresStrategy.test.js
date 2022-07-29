4c1b9b87ef8ad10b649973c1508a92561931b562
Miqueias Sousa <mi.queias2010@hotmail.com>
2022-07-28 23:26:08 -0300

    postgres delete method

--- 08-multi-db-postgres/src/tests/postgresStrategy.test.js
+++ 08-multi-db-postgres/src/tests/postgresStrategy.test.js
@@ -19,6 +19,7 @@ describe ('Postgres Strategy', function () {
 
     this.beforeAll(async function() {
         await context.connect()
+        await context.delete()
         await context.create(MOCK_HEROI_ATUALIZAR)
     })
 
@@ -55,5 +56,12 @@ describe ('Postgres Strategy', function () {
         const [itemAtualizado] = await context.read({ id: itemAtualizar.id })
         assert.deepEqual(result, 1)
         assert.deepEqual(itemAtualizado.nome, novoItem.nome)
+    })
+
+    it('remover por id', async function() {
+        const [item] = await context.read({})
+        const result = await context.delete(item.id)
+
+        assert.deepEqual(result, 1)
     })
 })
